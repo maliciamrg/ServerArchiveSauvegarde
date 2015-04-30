e2b8c85b1e88860246a5da3841f9c63d3eb27cc5
MaliciaMrg <malicia_mrg@hotmail.com>
2015-05-01 00:10:29 +0200

    init

---
+++ src/com/maliciamrg/archive/server/ServerArchiveSauvegarde.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.maliciamrg.archive.server;
+
+/**
+ * @author u75e018
+ *
+ */
+public class ServerArchiveSauvegarde {
+
+	/**
+	 * 
+	 */
+	public ServerArchiveSauvegarde() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
